aeebcaa93a212329025d08f7a5b3a7d0fc7fa61b
izaan.noman <izaan.nyc@gmail.com>
2024-02-10 01:24:54 -0500

    Add icons

--- pages/pages.py
+++ pages/pages.py
@@ -5,11 +5,12 @@ import tkinter.simpledialog
 
 import customtkinter
 
-from directory_manager import get_storage_dir
+from directory_manager import get_storage_dir, get_icon_dir
 from widgets.Buttons import DefaultButton, FolderObjectButton, FileObjectButton
 from widgets.CheckBox import TaskCheckBox
 from widgets.ErrorPopup import ErrorPopup
 from widgets.Page import Page
+from PIL import Image, ImageTk
 
 
 class TasksPage(Page):
@@ -21,13 +22,15 @@ class TasksPage(Page):
         top_buttons_frame = customtkinter.CTkFrame(self, fg_color='transparent')
         top_buttons_frame.pack(padx=10, pady=(0, 5), side='top', anchor='nw')
 
-        import_tasks_button = DefaultButton(top_buttons_frame, text="Import Tasks")
+        import_tasks_button = DefaultButton(top_buttons_frame, text="Import Tasks",
+                                            image=ImageTk.PhotoImage(Image.open(f'{get_icon_dir()}/download.png')))
         import_tasks_button.pack(padx=(0, 10), side='left', anchor='nw')
 
-        add_task_button = DefaultButton(top_buttons_frame, text="Add Task")
+        add_task_button = DefaultButton(top_buttons_frame, text="Add Task",
+                                        image=ImageTk.PhotoImage(Image.open(f'{get_icon_dir()}/plus-square.png')))
         add_task_button.pack(padx=(0, 10), side='right', anchor='nw')
 
-        tasks_scrollable_frame = customtkinter.CTkScrollableFrame(self)
+        tasks_scrollable_frame = customtkinter.CTkScrollableFrame(master=self, fg_color='#2c3e50')
         tasks_scrollable_frame.pack(fill='both', expand=True, padx=(5, 10), pady=(5, 5))
 
         # check_box = CheckListBox()
@@ -51,23 +54,28 @@ class CurriculumPage(Page):
         self.top_buttons_frame = customtkinter.CTkFrame(self, fg_color='transparent')
         self.top_buttons_frame.pack(padx=10, pady=(0, 5), side='top', anchor='nw')
 
-        self.add_folder_button = DefaultButton(self.top_buttons_frame, text="Add Folder", command=self.add_folder)
+        self.add_folder_button = DefaultButton(self.top_buttons_frame, text="Add Folder", command=self.add_folder,
+                                               image=ImageTk.PhotoImage(Image.open(f'{get_icon_dir()}/folder-plus.png')))
         self.add_folder_button.pack(padx=(0, 10), side='left', anchor='nw')
 
         self.create_folder_button = DefaultButton(self.top_buttons_frame, text="Create Folder",
-                                                  command=self.create_folder)
+                                                  command=self.create_folder,
+                                                  image=ImageTk.PhotoImage(Image.open(f'{get_icon_dir()}/folder-plus.png')))
         self.create_folder_button.pack(padx=(0, 10), side='left', anchor='nw')
 
-        self.clear_grid_button = DefaultButton(self.top_buttons_frame, text="Clear Grid", command=self.clear_grid)
+        self.clear_grid_button = DefaultButton(self.top_buttons_frame, text="Clear", command=self.clear_grid,
+                                               image=ImageTk.PhotoImage(Image.open(f'{get_icon_dir()}/trash.png')))
         self.clear_grid_button.pack(padx=(0, 10), side='left', anchor='nw')
 
-        self.refresh_grid_button = DefaultButton(self.top_buttons_frame, text="Refresh Grid", command=self.refresh_grid)
+        self.refresh_grid_button = DefaultButton(self.top_buttons_frame, text="Refresh", command=self.refresh_grid,
+                                                 image=ImageTk.PhotoImage(Image.open(f'{get_icon_dir()}/rotate-ccw.png')))
         self.refresh_grid_button.pack(padx=(0, 10), side='left', anchor='nw')
 
-        self.back_button = DefaultButton(self.top_buttons_frame, text="Back", command=self.go_back)
+        self.back_button = DefaultButton(self.top_buttons_frame, text="Back", command=self.go_back,
+                                         image=ImageTk.PhotoImage(Image.open(f'{get_icon_dir()}/arrow-left.png')))
         self.back_button.pack(padx=(0, 10), side='left', anchor='nw')
 
-        self.content_scrollable_frame = customtkinter.CTkScrollableFrame(master=self)
+        self.content_scrollable_frame = customtkinter.CTkScrollableFrame(master=self, fg_color='#2c3e50')
         self.content_scrollable_frame.pack(fill='both', expand=True, padx=5, pady=5)
 
         self.current_directory = get_storage_dir()

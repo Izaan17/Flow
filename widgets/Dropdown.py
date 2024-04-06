335ee83e97ad3e52630fdd5b43da2092561cc1cd
izaan.noman <izaan.nyc@gmail.com>
2024-04-06 02:03:18 -0400

    Change dropdown design

--- widgets/Dropdown.py
+++ widgets/Dropdown.py
@@ -7,7 +7,7 @@ class DefaultDropDown(customtkinter.CTkOptionMenu):
     def __init__(self, master: Any, label_text, **kwargs):
         self.frame = customtkinter.CTkFrame(master, fg_color='transparent')
         super().__init__(self.frame, **kwargs)
-        self.drop_down_label = customtkinter.CTkLabel(self.frame, text=label_text)
+        self.drop_down_label = customtkinter.CTkLabel(self.frame, text=label_text, font=('Roboto', 14))
         # Put frame on left side
-        self.frame.pack(anchor='w', pady=10)
-        self.drop_down_label.pack(side='left', padx=10)
+        self.frame.pack(anchor='w', pady=(0, 10), padx=5)
+        self.drop_down_label.pack(side='top', pady=5)

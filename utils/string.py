30e7cbe957e8b6c9f39007549398c75c3d9cff93
izaan.noman <izaan.nyc@gmail.com>
2024-05-21 03:32:07 -0400

    Add string.py

--- utils/string.py
+++ utils/string.py
@@ -3,3 +3,11 @@ def shorten_text(text, max_length):
         return text
     else:
         return text[:max_length - 3] + "..."  # truncate text and add ellipsis
+
+
+def remove_all_but_one_zero(s):
+    if '0' not in s:
+        return s
+
+    first_zero_index = s.index('0')
+    return s[:first_zero_index + 1].replace('0', '') + '0' + s[first_zero_index + 1:].replace('0', '')

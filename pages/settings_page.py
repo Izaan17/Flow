9a8e3a403c7182107c201f9a8902a11702209e98
izaan.noman <izaan.nyc@gmail.com>
2024-05-21 00:29:01 -0400

    Organize and clean up code

--- pages/settings_page.py
+++ pages/settings_page.py
@@ -1,8 +1,25 @@
+from functools import partial
 import customtkinter
-
 from utils.settings import settings
 from widgets.Dropdown import DefaultDropDown
 from widgets.Page import Page
+
+
+def create_dropdown(frame, label_text, values, command, setting_key, default_value):
+    dropdown = DefaultDropDown(frame, label_text=label_text, values=values, command=command)
+    dropdown.pack(anchor='w')
+    dropdown.set(settings.get_setting(setting_key, default_value))
+    return dropdown
+
+
+def on_setting_change(setting_key, choice):
+    settings.add_setting(setting_key, choice)
+
+
+def on_icon_size_change(choice):
+    width, height = choice.split("x")
+    settings.add_setting("icon_width", width)
+    settings.add_setting("icon_height", height)
 
 
 class SettingsPage(Page):
@@ -17,56 +34,30 @@ class SettingsPage(Page):
         self.drop_downs_frame = customtkinter.CTkFrame(self, fg_color='white')
         self.drop_downs_frame.pack(fill='both', expand=True)
 
-        self.max_items_per_row = DefaultDropDown(self.drop_downs_frame,
-                                                 label_text="Max Items Per Row",
-                                                 values=[str(i) for i in range(5, 11)], command=self.on_max_row_change)
-        self.max_items_per_row.pack(anchor='w')
-        self.max_items_per_row.set(settings.get_setting("max_items_per_row", 5))
+        # Create dropdowns with a generic handler
+        self.max_items_per_row = create_dropdown(
+            self.drop_downs_frame, "Max Items Per Row", [str(i) for i in range(5, 11)],
+            partial(on_setting_change, "max_items_per_row"), "max_items_per_row", 5
+        )
 
-        self.max_text_length = DefaultDropDown(self.drop_downs_frame,
-                                               label_text="Max Text Length",
-                                               values=[str(i) for i in range(10, 50)],
-                                               command=self.on_text_length_change)
-        self.max_text_length.pack(anchor='w')
-        self.max_text_length.set(settings.get_setting("max_text_length", 30))
+        self.max_text_length = create_dropdown(
+            self.drop_downs_frame, "Max Text Length", [str(i) for i in range(10, 50)],
+            partial(on_setting_change, "max_text_length"), "max_text_length", 30
+        )
 
-        self.show_image_preview = DefaultDropDown(self.drop_downs_frame,
-                                                  label_text="Show Image Preview",
-                                                  values=["True", "False"], command=self.on_img_preview_change)
-        self.show_image_preview.pack(anchor='w')
-        self.show_image_preview.set(settings.get_setting("show_img_preview", "True"))
+        self.show_image_preview = create_dropdown(
+            self.drop_downs_frame, "Show Image Preview", ["True", "False"],
+            partial(on_setting_change, "show_img_preview"), "show_img_preview", "True"
+        )
 
-        self.icon_size_drop_down = DefaultDropDown(self.drop_downs_frame, label_text="Icon Size",
-                                                   values=self.all_icon_sizes, command=self.on_icon_change)
+        self.icon_size_drop_down = DefaultDropDown(
+            self.drop_downs_frame, label_text="Icon Size", values=self.all_icon_sizes,
+            command=on_icon_size_change)
         self.icon_size_drop_down.pack(anchor='w')
-        # Set saved icon size or default
-        self.icon_size_drop_down.set(f"{settings.get_setting('icon_width', 50)}x"
-                                     f"{settings.get_setting('icon_height', 50)}")
+        self.icon_size_drop_down.set(
+            f"{settings.get_setting('icon_width', 50)}x{settings.get_setting('icon_height', 50)}")
 
-        self.allow_full_directory_deletion = DefaultDropDown(self.drop_downs_frame,
-                                                             label_text="Full Directory Deletion",
-                                                             values=["True", "False"], command=self.on_dir_delete)
-        self.allow_full_directory_deletion.pack(anchor='w')
-        self.allow_full_directory_deletion.set(settings.get_setting("allow_full_dir_deletion", "False"))
-
-    @staticmethod
-    def on_icon_change(choice):
-        icon_size = choice.split("x")
-        settings.add_setting("icon_width", icon_size[0])
-        settings.add_setting("icon_height", icon_size[1])
-
-    @staticmethod
-    def on_dir_delete(choice):
-        settings.add_setting("allow_full_dir_deletion", choice)
-
-    @staticmethod
-    def on_img_preview_change(choice):
-        settings.add_setting("show_img_preview", choice)
-
-    @staticmethod
-    def on_max_row_change(choice):
-        settings.add_setting("max_items_per_row", choice)
-
-    @staticmethod
-    def on_text_length_change(choice):
-        settings.add_setting("max_text_length", choice)
+        self.allow_full_directory_deletion = create_dropdown(
+            self.drop_downs_frame, "Full Directory Deletion", ["True", "False"],
+            partial(on_setting_change, "allow_full_dir_deletion"), "allow_full_dir_deletion", "False"
+        )

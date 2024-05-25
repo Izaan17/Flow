ba3cce5992722f5524d12fafecd20022cdf9133b
izaan.noman <izaan.nyc@gmail.com>
2024-05-24 20:37:28 -0400

    Revert open_file_folder windows changes.

--- utils/system.py
+++ utils/system.py
@@ -8,6 +8,6 @@ def get_file_system_app_name():
 def open_file_or_folder(file, reveal=False):
     arg = ""
     if reveal:
-        arg = "/select" if os.name == 'nt' else '-R'
-    command = f'start "" "{file}" {arg}' if os.name == "nt" else f'open {arg} "{file}"'
+        arg = '-R'
+    command = f'start "" "{file}"' if os.name == "nt" else f'open {arg} "{file}"'
     return os.system(command)

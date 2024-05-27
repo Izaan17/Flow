116211f5e1b50016493c468e01b437f47d31bc25
izaan.noman <izaan.nyc@gmail.com>
2024-05-27 01:56:07 -0400

    Make background white on default

--- widgets/Page.py
+++ widgets/Page.py
@@ -5,7 +5,7 @@ import customtkinter
 
 class Page(customtkinter.CTkFrame):
     def __init__(self, master: Any, page_title: str = "Page Title", subheading: str = "Subheading", *args, **kwargs):
-        super().__init__(master, **kwargs, corner_radius=0)
+        super().__init__(master, **kwargs, corner_radius=0, fg_color='white')
 
         self.page_title = customtkinter.CTkLabel(self, text=page_title, font=('Roboto', 36, 'bold'))
         self.page_title.pack(padx=10, pady=(50, 0), side='top', anchor='nw')

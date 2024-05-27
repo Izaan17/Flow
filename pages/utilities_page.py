ca4098da08ff52e68e4b6e8ece53b26517c119fb
izaan.noman <izaan.nyc@gmail.com>
2024-05-27 02:48:42 -0400

    Add placeholder coming soon text

--- pages/utilities_page.py
+++ pages/utilities_page.py
@@ -1,8 +1,11 @@
 from typing import Any
 
 from widgets.Page import Page
+import customtkinter
 
 
 class UtilitiesPage(Page):
     def __init__(self, master: Any, *args, **kwargs):
         super().__init__(master, *args, **kwargs, page_title="Utilities Page", subheading="Useful tools.")
+
+        customtkinter.CTkLabel(self, text="Coming Soon...", font=('Roboto', 48)).pack()

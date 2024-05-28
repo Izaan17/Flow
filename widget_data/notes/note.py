bf4fdc9ea2de6c88d9d882076ed3a45575f5202e
izaan.noman <izaan.nyc@gmail.com>
2024-05-28 00:20:59 -0400

    Add note class definition

---
+++ widget_data/notes/note.py
@@ -0,0 +1,35 @@
+import datetime
+
+
+class Note:
+    def __init__(self, title: str, content: str, date_created: datetime.datetime | None = None,
+                 date_modified: datetime.datetime | None = None):
+
+        self.title = title
+        self.content = content
+        self.date_created = date_created if date_created else datetime.datetime.now()
+        self.date_modified = date_modified if date_modified else self.date_created
+
+    def to_dict(self):
+        return {'title': self.title,
+                'content': self.content,
+                'date_created': self.date_created,
+                'date_modified': self.date_modified}
+
+    @staticmethod
+    def from_dict(data):
+        return Note(
+            data["title"],
+            data["content"],
+            data["note_id"],
+            datetime.datetime.fromisoformat(data["date_created"]),
+            datetime.datetime.fromisoformat(data["date_modified"])
+        )
+
+
+if __name__ == '__main__':
+    note = Note("Title", "Content")
+    print(note.title)
+    print(note.content)
+    print(note.date_created)
+    print(note.date_modified)

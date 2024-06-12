70bfdb2ea31321a9f6bab0d712c2546d387d7a25
izaan.noman <izaan.nyc@gmail.com>
2024-06-12 19:44:27 -0400

    Entries now have undo and redo functions along with the cut function.

--- utils/system.py
+++ utils/system.py
@@ -6,10 +6,18 @@ if os.name == "nt":
     import ctypes
     right_click_binding_key_code = "<Button-3>"
     file_system_app_name = "Explorer"
+    undo_button_sequence = "<Control-z>"
+    redo_button_sequence = "<Control-Shift-z>"
+    undo_button_name = "Control_L"
+    redo_button_name = "Control_R"
 
 elif os.name == "posix":
     right_click_binding_key_code = "<Button-2>"
     file_system_app_name = "Finder"
+    undo_button_sequence = "<Command-z>"
+    redo_button_sequence = "<Command-Shift-z>"
+    undo_button_name = "Meta_L"
+    redo_button_name = "Meta_R"
 
 
 def open_file_or_folder(file, reveal=False):
--- widgets/Entry.py
+++ widgets/Entry.py
@@ -1,6 +1,7 @@
 import tkinter
 from typing import Any
 import utils
+
 import customtkinter
 
 
@@ -8,25 +9,72 @@ class DefaultEntry(customtkinter.CTkEntry):
     def __init__(self, master: Any, **kwargs):
         super().__init__(master, **kwargs)
         self.right_click_menu = tkinter.Menu(self, tearoff=0)
+        self.right_click_menu.add_command(label="Cut", command=self._cut_text)
         self.right_click_menu.add_command(label="Copy", command=self._copy_text)
         self.right_click_menu.add_command(label="Paste", command=self._paste_text)
         self.right_click_menu.add_command(label="Select All", command=self._select_all)
 
         self.bind(utils.system.right_click_binding_key_code,
                   lambda event: self.right_click_menu.tk_popup(event.x_root, event.y_root))
+        self._update_context_menu()
+        self.bind("<KeyRelease>", self._update_context_menu)
+        self.bind("<ButtonRelease>", self._update_context_menu)
+        self.bind(utils.system.undo_button_sequence, self.undo)
+        self.bind(utils.system.redo_button_sequence, self.redo)
+        self.bind("<Key>", self.on_key_press)
+
+        self.undo_stack = []
+        self.redo_stack = []
+
+    def undo(self, event=None):
+        if self.undo_stack:
+            text = self.undo_stack.pop()
+            self.redo_stack.append(self.get())
+            self.delete(0, tkinter.END)
+            self.insert(0, text)
+
+    def redo(self, event=None):
+        if self.redo_stack:
+            text = self.redo_stack.pop()
+            self.undo_stack.append(self.get())
+            self.delete(0, tkinter.END)
+            self.insert(0, text)
+
+    def on_key_press(self, event):
+        print(event.keysym)
+        if event.keysym not in [utils.system.undo_button_name, utils.system.redo_button_name, "z", "y"]:
+            self.undo_stack.append(self.get())
+            self.redo_stack.clear()
+
+    def _cut_text(self):
+        if self.select_present():
+            self._copy_text()
+            self.delete("sel.first", "sel.last")
 
     def _copy_text(self):
-        try:
+        if self.select_present():
             selection = self.selection_get()
-        except tkinter.TclError:
+        else:
             selection = self.get()
-        self.master.clipboard_clear()
-        self.master.clipboard_append(selection)
+        self.clipboard_clear()
+        self.clipboard_append(selection)
 
     def _paste_text(self):
         self.delete(0, tkinter.END)
-        self.insert(0, self.master.clipboard_get())
+        self.insert(0, self.clipboard_get())
 
     def _select_all(self):
         self.focus_set()
         self.select_range(0, tkinter.END)
+
+    def _update_context_menu(self, event=None):
+        try:
+            if self.select_present():
+                self.right_click_menu.entryconfig(0, state="normal")
+                self.right_click_menu.entryconfig(1, state="normal")
+            else:
+                self.right_click_menu.entryconfig(0, state="disabled")
+                self.right_click_menu.entryconfig(1, state="disabled")
+        except tkinter.TclError:
+            # Catch any errors in case the menu item labels are not found
+            pass

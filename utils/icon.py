fd3bb43121e386388144828e1187b6844e41b1ea
izaan.noman <izaan.nyc@gmail.com>
2024-06-14 14:39:52 -0400

    Change default icon size

--- utils/icon.py
+++ utils/icon.py
@@ -5,7 +5,7 @@ from PIL import Image
 import utils.directory_manager
 
 
-def load_icon(icon_name: str, size: tuple[int, int] = (20, 20)):
+def load_icon(icon_name: str, size: tuple[int, int] = (22, 22)):
     return customtkinter.CTkImage(
         light_image=Image.open(f'{utils.directory_manager.get_icon_dir()}{os.sep}{icon_name}'),
         size=size)

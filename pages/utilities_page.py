44ccd9fc0c8a53e9f6a328b1403db255ceb48f95
izaan.noman <izaan.nyc@gmail.com>
2024-06-14 01:15:47 -0400

    You can now separately run each page for testing.

--- pages/utilities_page.py
+++ pages/utilities_page.py
@@ -1,5 +1,9 @@
 from typing import Any
-
+from widgets.popups.validation.widget_data_validator import NumericValidator
+from widgets.UtilitySection import UtilitySection
+from widgets.Entry import DefaultEntry
+from widgets.Buttons import DefaultButton
+from utils.icon import load_icon
 from widgets.Page import Page
 import customtkinter
 
@@ -8,4 +12,61 @@ class UtilitiesPage(Page):
     def __init__(self, master: Any, *args, **kwargs):
         super().__init__(master, *args, **kwargs, page_title="Utilities Page", subheading="Useful tools.")
 
-        customtkinter.CTkLabel(self, text="Coming Soon...", font=('Roboto', 48)).pack()
+        self.main_frame = customtkinter.CTkFrame(self, fg_color='transparent')
+
+        self.grade_calculator_page_section = UtilitySection(self.main_frame, "Grade Calculator",
+                                                            icon=load_icon('file-text.png', (35, 35)))
+        self.grade_calculator_page_section.pack(side='left', expand=True, fill='both')
+
+        self.final_grade_simulator_section = UtilitySection(self.main_frame, section_title="Final Grade Simulator",
+                                                            section_subtitle="Use this tool to find out the grade "
+                                                                             "needed on the final exam in order to "
+                                                                             "get a desired grade in a course.")
+        self.final_grade_simulator_section.pack(side='right', expand=True, fill='both')
+
+        self.entries_grid = customtkinter.CTkFrame(self.final_grade_simulator_section, fg_color='transparent')
+        self.entries_grid.pack(pady=20)
+
+        self.current_grade_entry = DefaultEntry(self.entries_grid, placeholder_text="Current Grade",
+                                                validators=[NumericValidator()])
+        self.current_grade_entry.grid(row=0, column=0, padx=5, pady=5)
+
+        self.desired_grade_entry = DefaultEntry(self.entries_grid, placeholder_text="Desired Grade",
+                                                validators=[NumericValidator()])
+        self.desired_grade_entry.grid(row=0, column=1, padx=5, pady=5)
+
+        self.final_exam_weight_entry = DefaultEntry(self.entries_grid, placeholder_text="Final Weight",
+                                                    validators=[NumericValidator()])
+        self.final_exam_weight_entry.grid(row=0, column=2, padx=5, pady=5)
+
+        self.simulate_button = DefaultButton(self.final_grade_simulator_section, text="Simulate",
+                                             command=self.simulate_grade)
+        self.simulate_button.pack(pady=10)
+
+        self.simulation_result = customtkinter.CTkLabel(self.final_grade_simulator_section, text="")
+        self.simulation_result.pack()
+
+        self.main_frame.pack(expand=True, fill='both')
+
+    def simulate_grade(self):
+        current_grade = int(self.current_grade_entry.validated_get())
+        desired_grade = int(self.desired_grade_entry.validated_get())
+        final_exam_weight = int(self.final_exam_weight_entry.validated_get())
+        if current_grade and desired_grade and final_exam_weight:
+            # Convert final grade to decimal
+            final_exam_weight /= 100.0
+
+            # Calculate the grade needed on the final exam
+            needed_final_exam_grade = (desired_grade - current_grade * (1 - final_exam_weight)) / final_exam_weight
+
+            self.simulation_result.configure(text=f"You need to get a grade of {needed_final_exam_grade:.2f} or "
+                                                  f"higher on your final.")
+
+
+if __name__ == "__main__":
+    app = customtkinter.CTk()
+
+    page = UtilitiesPage(app)
+    page.pack(expand=True, fill='both')
+
+    app.mainloop()
